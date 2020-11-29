238643b2979bc381d3ed864d8090367dc49184dc
Laurens Bultynck <laurens@starringjane.com>
2020-11-29 22:21:32 +0100

    Use trait on component

--- src/Component.php
+++ src/Component.php
@@ -6,10 +6,12 @@ use Illuminate\View\Component as BaseComponent;
 
 abstract class Component extends BaseComponent
 {
+    use RendersBlade;
+
     protected function view($view = null, $data = [], $mergeData = [])
     {
         $data = array_merge($data, $this->data());
 
-        return WordpressBlade::getInstance()->make($view, $data, $mergeData);
+        return $this->blade()->make($view, $data, $mergeData);
     }
 }
